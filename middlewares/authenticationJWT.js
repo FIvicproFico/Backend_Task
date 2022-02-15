d90ccd7407b48cd82fd3a254ad193b3e342641d7
FIvicproFico <fivic@profico.hr>
2022-02-15 15:33:32 +0100

    TODO: Extra security check in authenticationJWT middleware

--- middlewares/authenticationJWT.js
+++ middlewares/authenticationJWT.js
@@ -1,26 +1,37 @@
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
-const {accessTokenSecret, refreshTokenSecret, refreshTokens} = require('../constants/tokens')
+const {
+  accessTokenSecret,
+  refreshTokenSecret,
+  refreshTokens,
+} = require("../constants/tokens");
 
 const authenticateJWT = (req, res, next) => {
+  console.log("MIDDLEWARE: \t AUTHENTICATION");
+  console.log(
+    "Req. headers: \t " +
+      JSON.stringify(
+        req.headers.authorization.slice(0, 17) +
+          "..." +
+          req.headers.authorization.slice(-10)
+      )
+  );
+  const authHeader = req.headers.authorization;
 
-    console.log("MIDDLEWARE: \t AUTHENTICATION")
-    console.log("Req. headers: \t " + JSON.stringify(req.headers.authorization.slice(0,17) + "..." + req.headers.authorization.slice(-10)))
-    const authHeader = req.headers.authorization;
-    
-    if (authHeader) {
-        const token = authHeader.split(' ')[1]
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
 
-        jwt.verify(token, accessTokenSecret, (err, user) => {
-            if (err) {
-                return res.sendStatus(403)
-            }
-            res.locals.user = user
-            next()
-        })
-    } else {
-        res.sendStatus(401)
-    }
+    jwt.verify(token, accessTokenSecret, (err, user) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+      res.locals.user = user;
+      //Another check if User is in Database (in meanwhile user could have been deleted but user token will stay active)
+      next();
+    });
+  } else {
+    res.sendStatus(401);
+  }
 };
 
-module.exports = authenticateJWT
+module.exports = authenticateJWT;
